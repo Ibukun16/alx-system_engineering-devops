ff1bff9b290fd0c3db5f9b0a973312d07177bf54
Ibukun16 <ib.agunbiade18@gmail.com>
2024-07-06 03:18:38 -0700

    Update ssh task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,13 @@
 # A Puppet script that creates ssh config file
-include stdlib
 
 file_line { 'Turn off passwd auth':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => '	 PasswordAuthenticatication no',
-  replace => true,
+  line   => '	 PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '	 IdentityFile ~/.ssh/school',
-  replace => true,
 }
