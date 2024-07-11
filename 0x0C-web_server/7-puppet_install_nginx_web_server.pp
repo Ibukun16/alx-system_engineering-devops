0b36c9448661fc05291fca4c3fc2af002e2a6404
Ibukun16 <ib.agunbiade18@gmail.com>
2024-07-11 15:18:25 -0700

    puppet manifest web server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,36 @@
+# Automate Installation of nginx server on ubuntu web-01 server us puppet manifest
+# Nginx should be listening on port 80
+# Return a page that contains "Hello World" when querying
+# Nginx at its root with Get request using curl
+# Configure your Nginx server so that /redirect_me is redirecting to another page.
+# The redirection must be a “301 Moved Permanently”
+
+
+exec { 'apt-get-update':
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure => 'installed',
+}
+
+service { 'nginx':
+  ensure  => 'running',
+  require => file_line['perform a redirection'],
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+  ensure  => 'present',
+  content => 'Hello World!',
+  require => Package['nginx']
+}
+
+file_line { 'perform a redirection':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-enabled/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me/$ https://www.jw.org/en/library/videos/intros-for-the-ministry/jehovahs-witnesses-who-are-we-intro permanent;',
+  after   => 'root /var/www/html;',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
