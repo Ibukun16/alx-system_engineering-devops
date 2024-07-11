11b622a80acd61959ee86f8b0958ac509fab8b9b
Ibukun16 <ib.agunbiade18@gmail.com>
2024-07-11 03:54:01 -0700

    web server project

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,35 @@
+# Puppet script to install nginx 
+# Install nginx server on ubuntu web-01 server
+# Nginx should be listening on port 80
+# Return a page that contains "Hello World" when querying
+# Nginx at its root with Get request using curl
+# Configure your Nginx server so that /redirect_me is redirecting to another page.
+# The redirection must be a “301 Moved Permanently”
+# Configure your Nginx server to have a custom 404 page that
+# contains the string Ceci n'est pas une page
+# The page must return an HTTP 404 error code
+# The page must contain the string Ceci n'est pas une page
+
+package {'nginx':
+  ensure => 'present',
+}
+
+exec {'install':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
+
+}
+
+exec {'Hello':
+  command  => 'echo "Hello World!" | dd status=none of=/var/www/html/index.nginx-debian.html',
+  provider => shell,
+}
+
+exec {'sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.jw.org\/en\/library\/videos\/intros-for-the-ministry\/jehovahs-witnesses-who-are-we-intro permanent;" /etc/nginx/sites-enabled/default':
+  provider => shell,
+}
+
+exec {'run':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
