ad57733cd7a9092b8327ee78b7be470d49e02626
Ibukun16 <ib.agunbiade18@gmail.com>
2024-07-15 05:53:46 -0700

    Automate custom header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,39 +1,16 @@
-# Creating atom HTTP header with Puppet
+# Automate the creation of a custom HTTP header with Puppet
 
 exec {'update':
-  provider => shell,
-  command  => '/usr/bin/sudo apt-get -y pdate',
-  before   => Exec['install Nginx'],
+  command => '/usr/bin/apt-get update',
 }
-
-package { 'nginx':
-  ensure  => installed,
-  require => Exec['update'],
+-> package {'nginx':
+  ensure => 'present',
 }
-
-file_line { 'redirect':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-enabled/default',
-  after   => 'listen 80 default_server;',
-  line    => 'rewrite ^/redirect_me https://github.com/Ibukun16 permanent;',
-  require => Package['nginx'],
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
-
-file_line { 'addHeader':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-enabled/default',
-  after   => 'listen 80 default_server;',
-  line    => 'add_header X-Served-By $hostname;',
-  require => Package['nginx'],
-}
-
-file { '/var/www/html/index.nginx-debian.html':
-  content => 'Hello World!',
-  require => Package['nginx'],
-}
-
-exec { 'run':
-  provider => shell,
-  command  => 'sudo service nginx restart'
-  require  => Package['nginx'],
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
 }
