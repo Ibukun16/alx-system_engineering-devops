eefde914fc0d47b195c5debec21c694eb701e027
Ibukun16 <ib.agunbiade18@gmail.com>
2024-07-30 10:43:12 -0700

    Updating task 2 of API integration to web development

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,2 +1,25 @@
 #!/usr/bin/python3
-"""A script that build on the previous task which uses REST"""
+"""A script that build on the previous task which uses REST API
+to obtain information about an employee TODO list then export the
+data in the JSON format.
+"""
+
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    userID = argv[1]
+    baseURL = 'https://jsonplaceholder.typicode.com/users'
+    userNAME = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+            .format(userID)).json()
+    tasks = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+            .format(argv[1])).json()
+
+    with open("{}.json".format(userID), "w") as USER_ID:
+        json.dump({userID: [{
+            "task": task.get('title'),
+            "completed": task.get('completed'),
+            "username": userNAME.get('username')
+            } for task in tasks]}, USER_ID)
