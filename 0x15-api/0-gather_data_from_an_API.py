b5d3de7a9caa432004b9b157eb4aab6464255e1d
Ibukun16 <ib.agunbiade18@gmail.com>
2024-07-29 19:50:08 -0700

    Updating API integration to web development

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,9 +13,8 @@ if __name__ == "__main__":
     session_quest = requests.Session()
 
     empID = argv[1]
-    baseURL = 'https://jsonplaceholder.typicode.com/users'
-    empURL = baseURL + '/' + empID
-    todoURL = baseURL + '/' + empID + '/todos'
+    empURL = 'https://jsonplaceholder.typicode.com/users/{}'.format(empID)
+    todoURL = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(empID)
 
     responsename = session_quest.get(empURL)
     empNAME = responsename.json()['name']
@@ -24,15 +23,14 @@ if __name__ == "__main__":
     tasks = responsetodo.json()
 
     completeTasks = 0
-    done_tasks = []
 
     for task in tasks:
         if task['completed']:
-            done_tasks.append(task)
             completeTasks += 1
 
     print("Employee {} is done with tasks({}/{}):"
           .format(empNAME, completeTasks, len(tasks)))
 
-    for task in done_tasks:
-        print("\t " + task.get('title'))
+    for task in tasks:
+        if task['completed']:
+            print("\t " + task.get('title'))
