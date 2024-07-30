d2909951b529569533757574bb26cabb06c0aa54
Ibukun16 <ib.agunbiade18@gmail.com>
2024-07-29 23:55:27 -0700

    Export to JSON file

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""A script that build on the previous task which uses REST API
+to obtain information about an employee TODO list then export the
+data in the CSV format.
+"""
+
+import csv
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    userID = argv[1]
+    baseURL = 'https://jsonplaceholder.typicode.com/users'
+    userNAME = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                            .format(userID)).json()
+    tasks = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                         .format(argv[1])).json()
+
+    with open("{}.json".format(userID), "w") as userid:
+        json.dump({userID: [{
+                "task": task.get('title'),
+                "completed": task.get('completed'),
+                "username": userNAME.get('username')
+            } for task in tasks]}, userid)
