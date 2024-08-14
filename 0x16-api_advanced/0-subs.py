5c881a25185b307d30ae7b172cdc23eaf63c50de
Ibukun16 <ib.agunbiade18@gmail.com>
2024-08-14 04:24:08 -0700

    Advanced api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 """A Function that query subscribers on a given Reddit sureddit."""
 import requests
+import json
 
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given reddit."""
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    user = {"User-agent": "Google Chrome Version 127.0.6533.100"}
-    response = requests.get(url, headers=user, allow_redirects=False)
-    if response.status_code == 200:
+    user_agent = {"User-Agent": "Google Chrome Version 127.0.6533.120"}
+    response = requests.get(url, headers=user_agent)
+    try:
         return response.json().get("data").get("subscribers")
-    return 0
+
+    except Exception:
+        return 0
