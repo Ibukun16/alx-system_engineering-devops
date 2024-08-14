0cd24b959062556f9068643390c8eb30ec20a06e
Ibukun16 <ib.agunbiade18@gmail.com>
2024-08-14 04:28:39 -0700

    Advanced api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
 """A Function that query subscribers on a given Reddit sureddit."""
 import requests
-import json
 
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given reddit."""
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    user_agent = {"User-Agent": "Google Chrome Version 127.0.6533.120"}
-    response = requests.get(url, headers=user_agent)
-    try:
+    user = {"User-Agent": "Google Chrome Version 127.0.6533.120"}
+    response = requests.get(url, headers=user_agent, allow_redirects=False)
+    if response.staus_code == 200:
         return response.json().get("data").get("subscribers")
-
-    except Exception:
-        return 0
+    return 0
