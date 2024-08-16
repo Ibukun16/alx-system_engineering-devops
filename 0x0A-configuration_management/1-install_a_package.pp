6946b67c81b16200b38de490f55f1376fce32fa6
Ibukun16 <ib.agunbiade18@gmail.com>
2024-08-16 00:56:38 -0700

    Updating api

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#!/usr/bin/pup
-#Install a specific version of flask from pip3
-package {'flask':
+#Using puppet, install a specific version of flask from pip3
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
