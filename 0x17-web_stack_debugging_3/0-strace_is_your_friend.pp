2de7c93bc94d250008a09ad874d605b90d5204d5
Ibukun16 <ib.agunbiade18@gmail.com>
2024-08-16 09:58:27 -0700

    web debugging

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# A puppet manuscript to fix a broken line in a file on a server
+#replace line containing "phpp" with "php"
+exec{ 'fix-wordpress':
+    command => "sudo sed -i \'s/.phpp/.php/g\' /var/www/html/wp-settings.php",
+    path    => ['/bin','/usr/bin']
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,1 @@
+This directory uses strace to solve a challenge associated with debugging the failure of apache web server. Web debugging is an essetial skill for a system devops engineer
