0b488826b459f56cb410da4271f26fe812a7099d
Ibukun16 <ib.agunbiade18@gmail.com>
2024-08-17 10:10:32 -0700

    Advanced api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,12 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given reddit."""
     url = f"https://oauth.reddit.com/r/{subreddit}/about.json"
     user = {"User-Agent": "Google Chrome Version 127.0.6533.120"}
-    response = requests.get(url, headers=user, allow_redirects=False)
-    if response.status_code == 200:
-        return response.json().get("data").get("subscribers")
-    return 0
+
+    try:
+        response = requests.get(url, headers=user, allow_redirects=False)
+        response.raise_for_status()
+
+        data = response.json()
+        return data['data']['subscribers']
+    except requests.exceptions.RequestException:
+        return 0
