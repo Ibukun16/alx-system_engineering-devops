155be766b251725490b6bbd34b9a2c8a15664987
Ibukun16 <ib.agunbiade18@gmail.com>
2024-08-17 08:08:33 -0700

    Advanced api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 """A Function that query subscribers on a given Reddit sureddit."""
 import requests
-import json
 
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given reddit."""
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
     url = f"https://oauthi.reddit.com/r/{subreddit}/about.json"
     user = {"User-Agent": "Google Chrome Version 127.0.6533.120"}
     response = requests.get(url, headers=user, allow_redirects=False)
