849d89cd1e6839f57b02fc37ec4f4e1cff377a7e
Ibukun16 <ib.agunbiade18@gmail.com>
2024-08-17 09:59:48 -0700

    Advanced api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,11 +7,25 @@ import json
 
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on agiven subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot/.json?limit=10"
-    user = {"User-Agent": "Google Chrome Version 127.0.6533.120"}
-    response = requests.get(url, headers=user, allow_redirects=False)
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "Google Chrome Version 127.0.6533.120"
+    }
+
+    params = {
+        "limit": 10
+    }
+
+    response = requests.get(
+        url,
+        headers=headers,
+        params=params,
+        allow_redirects=False
+    )
+
     if response.status_code != 200:
         print("None")
         return
+
     res = response.json().get("data")
     [print(c.get("data").get("title")) for c in res.get("children")]
