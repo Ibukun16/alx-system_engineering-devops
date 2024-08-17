f8c90788c8f4f91bf6a8b7fe930c5fa90366edb2
Ibukun16 <ib.agunbiade18@gmail.com>
2024-08-17 07:57:30 -0700

    Advanced api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,9 +7,7 @@ import json
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given reddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    user = {
-        "User-Agent": "linux:0x16.api.advanced:v20.4.49 (by /u/Ibukun16)"
-    }
+    user = {"User-Agent": "Google Chrome v127.0.6533.120"}
     response = requests.get(url, headers=user, allow_redirects=False)
     if response.status_code == 200:
         return response.json().get("data").get("subscribers")
